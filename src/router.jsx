535411c0745a1699cce24f024e7692aea2a069a2
cunpoi <cunpoi1357@gmail.com>
2023-01-26 22:30:08 +0700

    fix route

--- src/router.jsx
+++ src/router.jsx
@@ -20,89 +20,94 @@ import Game from './Page/NguoiDung/Game'
 import BotTuDong from './Page/BotTuDong'
 import ChatBox from './Page/ChatBox'
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+    [
+        {
+            element: <DefaultLayout />,
+            children: [
+                {
+                    path: '/',
+                    element: <Home />
+                }
+            ]
+        },
+        {
+            path: 'admin',
+            element: <AdminLayout />,
+            children: [
+                {
+                    index: true,
+                    element: <Dashboard />
+                },
+                {
+                    path: 'nguoi-dung',
+                    children: [
+                        {
+                            path: 'danh-sach',
+                            element: <DSNguoiDung />
+                        },
+                        {
+                            path: 'chinh-sua',
+                            element: <ChinhSuaNguoiDung />
+                        },
+                        {
+                            path: 'lich-su',
+                            element: <LSThayDoiSD />
+                        },
+                        {
+                            path: 'game',
+                            element: <Game />
+                        }
+                    ]
+                },
+                {
+                    path: 'lich-su-rut-xu',
+                    element: <LichSuRutXu />
+                },
+                {
+                    path: 'chan-le',
+                    children: [
+                        { path: '', element: <PhienChanLe /> },
+                        {
+                            path: 'nguoi-choi',
+                            element: <NguoiChoi />
+                        },
+                        {
+                            path: 'top',
+                            element: <Top />
+                        }
+                    ]
+                },
+                {
+                    path: 'bot-tu-dong',
+                    element: <BotTuDong />
+                },
+                {
+                    path: 'ti-le-thuong',
+                    element: <TiLeThuong />
+                },
+                {
+                    path: 'nap-rut-xu',
+                    element: <NapRutXu />
+                },
+                {
+                    path: 'quan-li-may-chu',
+                    element: <QuanLiMayChu />
+                },
+                {
+                    path: 'cau-hinh-website',
+                    element: <CauHinhWebsite />
+                },
+                {
+                    path: 'chat-box',
+                    element: <ChatBox />
+                }
+            ]
+        }
+    ],
     {
-        element: <DefaultLayout />,
-        children: [
-            {
-                path: '/',
-                element: <Home />
-            }
-        ]
-    },
-    {
-        path: 'admin',
-        element: <AdminLayout />,
-        children: [
-            {
-                index: true,
-                element: <Dashboard />
-            },
-            {
-                path: 'nguoi-dung',
-                children: [
-                    {
-                        path: 'danh-sach',
-                        element: <DSNguoiDung />
-                    },
-                    {
-                        path: 'chinh-sua',
-                        element: <ChinhSuaNguoiDung />
-                    },
-                    {
-                        path: 'lich-su',
-                        element: <LSThayDoiSD />
-                    },
-                    {
-                        path: 'game',
-                        element: <Game />
-                    }
-                ]
-            },
-            {
-                path: 'lich-su-rut-xu',
-                element: <LichSuRutXu />
-            },
-            {
-                path: 'chan-le',
-                children: [
-                    { path: '', element: <PhienChanLe /> },
-                    {
-                        path: 'nguoi-choi',
-                        element: <NguoiChoi />
-                    },
-                    {
-                        path: 'top',
-                        element: <Top />
-                    }
-                ]
-            },
-            {
-                path: 'bot-tu-dong',
-                element: <BotTuDong />
-            },
-            {
-                path: 'ti-le-thuong',
-                element: <TiLeThuong />
-            },
-            {
-                path: 'nap-rut-xu',
-                element: <NapRutXu />
-            },
-            {
-                path: 'quan-li-may-chu',
-                element: <QuanLiMayChu />
-            },
-            {
-                path: 'cau-hinh-website',
-                element: <CauHinhWebsite />
-            },
-            {
-                path: 'chat-box',
-                element: <ChatBox />
-            }
-        ]
+        basename: '/'
     }
-])
+)
 
 export default router
